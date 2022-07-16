1c80c123373be380bf6f085ef99abdf25a9734c2
RosalieWessels <rosalie.wessels@yahoo.com>
2022-07-16 10:04:33 -0700

    Initial Commit

---
+++ Shared/affirmations_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  affirmations_AppApp.swift
+//  Shared
+//
+//  Created by Rosalie Wessels on 7/16/22.
+//
+
+import SwiftUI
+
+@main
+struct affirmations_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
