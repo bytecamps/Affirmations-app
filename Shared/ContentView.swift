d8d67139a2fe748f8994d6ed2c2b7e95933c2fc3
RosalieWessels <rosalie.wessels@yahoo.com>
2022-07-16 15:59:51 -0700

    app commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var affirmation = ""
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Spacer()
+            Text("\(affirmation)")
+                .font(.system(size: 40, weight: .semibold, design: .rounded))
+                .onAppear(perform: {
+                    getAffirmation()
+            })
+            Spacer()
+            Button(action: {getAffirmation()}) {
+                Text("Get another")
+                    .font(.system(size: 20, weight: .regular, design: .rounded))
+                    .foregroundColor(.white)
+            }
             .padding()
+            .background(Color.green)
+            .cornerRadius(20)
+            Spacer()
+            
+        }
+        .padding(.horizontal)
+    }
+
+    
+    func getAffirmation() {
+        loadData { (receivedAffirmation) in
+            affirmation = receivedAffirmation.affirmation
+        }
+    }
+    
+    func loadData(completion: @escaping (Affirmation) ->  ()) {
+        
+        guard let url = URL(string: "https://www.affirmations.dev") else {
+            print("Invalid URL")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            let result = try! JSONDecoder().decode(Affirmation.self, from: data!)
+            
+            DispatchQueue.main.async {
+                print(result)
+                completion(result)
+            }
+        }
+        .resume()
     }
 }
 
@@ -18,4 +62,8 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Affirmation : Codable {
+    var affirmation : String
 }
